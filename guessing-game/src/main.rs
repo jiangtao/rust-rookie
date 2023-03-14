c5288f99d9f741bc4116d1b09d11c5e1fff5f55a
jiangtao <w3cfuns@qq.com>
2023-03-14 10:36:01 +0800

    chore: rust one day 3.13

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,6 +1,8 @@
 // https://kaisery.github.io/trpl-zh-cn/ch02-00-guessing-game-tutorial.html
 use rand::Rng;
 use std::io; // Import the io module 相当于 Node.js的require
+// :: 和 . 的区别是什么, 什么时候用. 什么时候用::
+// 得克服心理作用, 把以前学的 js 都忘掉, 毕竟 rust 确实读起来好别扭啊 😳
 fn main() {
     println!("猜数字啊?"); // console.log
     println!("输入个数字?"); // console.log
@@ -10,6 +12,7 @@ fn main() {
      * :: 相当于访问符号, 就是 javascript .
      * & 引用，指针，可以改变引用的值
      * &mut 可变引用，指针，可以改变引用的值
+     * 可以用 let mut x = 5; 来声明可变变量
      */
     let mut guess = String::new();
     let secret_number = rand::thread_rng().gen_range(1..201);
